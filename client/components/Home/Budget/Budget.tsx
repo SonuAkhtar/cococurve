715cc50fd61aac74e921600cd32f25a0fbe50eb3
SonuA <sonua981@gmail.com>
2024-12-13 13:26:54 +0530

    minor UI fixes

--- client/components/Home/Budget/Budget.tsx
+++ client/components/Home/Budget/Budget.tsx
@@ -15,19 +15,21 @@ const Budget = () => {
           <h4 className={styles.title}>FASHION UNDER BUDGET</h4>
           <h1 className={styles.offer}>BUDGET UNDER 999.</h1>
         </div>
-        <div className={styles.cards}>
-          {budgetData.map((item) => (
-            <BudgetCard
-              key={item.id}
-              id={item.id}
-              image={item.image}
-              rating={item.rating}
-              title={item.title}
-              newPrice={item.newPrice}
-              oldPrice={item.oldPrice}
-              off={item.off}
-            />
-          ))}
+        <div className={styles.cards_wrapper}>
+          <div className={styles.cards}>
+            {budgetData.map((item) => (
+              <BudgetCard
+                key={item.id}
+                id={item.id}
+                image={item.image}
+                rating={item.rating}
+                title={item.title}
+                newPrice={item.newPrice}
+                oldPrice={item.oldPrice}
+                off={item.off}
+              />
+            ))}
+          </div>
         </div>
         <Button text="View All" />
       </div>
--- client/components/Home/Sale/Sale.tsx
+++ client/components/Home/Sale/Sale.tsx
@@ -16,19 +16,21 @@ const Sale = () => {
           <h1 className={styles.offer}>FLAT 20% OFF*</h1>
           <h3 className={styles.subtitle}>On all women apparels</h3>
         </div>
-        <div className={styles.cards}>
-          {saleData.map((item) => (
-            <SaleCard
-              key={item.id}
-              id={item.id}
-              image={item.image}
-              rating={item.rating}
-              title={item.title}
-              newPrice={item.newPrice}
-              oldPrice={item.oldPrice}
-              off={item.off}
-            />
-          ))}
+        <div className={styles.cards_wrapper}>
+          <div className={styles.cards}>
+            {saleData.map((item) => (
+              <SaleCard
+                key={item.id}
+                id={item.id}
+                image={item.image}
+                rating={item.rating}
+                title={item.title}
+                newPrice={item.newPrice}
+                oldPrice={item.oldPrice}
+                off={item.off}
+              />
+            ))}
+          </div>
         </div>
         <Button text="View All" />
       </div>
