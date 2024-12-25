7bff76a897f33bb67cb7c6b67814d7e3a1bd57f3
SonuA <sonua981@gmail.com>
2024-12-25 21:11:16 +0530

    order confirm, order wrong pages created

---
+++ client/app/order/wrong/page.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import styles from "./page.module.scss"; // SCSS
+
+// fontawesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopy } from "@fortawesome/free-regular-svg-icons";
+
+// components
+import Button from "@/components/Button/Button";
+
+const OrderWrong = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.error_box}>
+          <div className={styles.error_icon}>
+            <Image
+              src="/Images/order/wrong/order-wrong.png"
+              alt="error image"
+              width={0}
+              height={0}
+              sizes="100vw"
+            />
+          </div>
+          <div className={styles.error_details}>
+            <h2>Something has went wrong!</h2>
+            <h5>
+              For more details write us at <span>support@cococurve.in</span>
+            </h5>
+          </div>
+          <Button text="Go back" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OrderWrong;
